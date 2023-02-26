57f9d3ac8059e771044c1a65dff3f9731a326c56
Lennox Charles <lennyaiko17@gmail.com>
2023-02-26 21:19:41 +0100

    bug fixes and built DB|

--- models.py
+++ models.py
@@ -1,5 +1,9 @@
 from datetime import datetime
+
+from marshmallow_sqlalchemy import fields
+
 from config import db, ma
+
 
 class Note(db.Model):
     __tablename__ = "note"
@@ -10,27 +14,43 @@ class Note(db.Model):
         db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow
     )
 
+
+class NoteSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = Note
+        load_instance = True
+        sqla_session = db.session
+        include_fk = True
+
+
 class Person(db.Model):
-    __tablename__ = "Person"
+    __tablename__ = "person"
     id = db.Column(db.Integer, primary_key=True)
     lname = db.Column(db.String(32), unique=True)
     fname = db.Column(db.String(32))
     timestamp = db.Column(
         db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow
     )
+
     notes = db.relationship(
         Note,
-        bckref="person",
+        backref="person",
         cascade="all, delete, delete-orphan",
-        single_parent=True, # not to allow any orphan note. this works with delete orphan
-        order_by="desc(Note.timestamp)"
+        single_parent=True,
+        order_by="desc(Note.timestamp)",
     )
+
 
 class PersonSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
         model = Person
         load_instance = True
         sqla_session = db.session
+        include_relationships = True
 
+    notes = fields.Nested(NoteSchema, many=True)
+
+
+note_schema = NoteSchema()
 person_schema = PersonSchema()
 people_schema = PersonSchema(many=True)
